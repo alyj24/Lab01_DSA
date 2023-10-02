d94e6484a8ce7004c74eeeded1b5b745bc0e19fb
Family <albon.136546120855@2depedqc.ph>
2023-10-02 12:43:54 +0800

    Done

--- delete_duplication.py
+++ delete_duplication.py
@@ -1,31 +1,25 @@
-def print_header(text, color_code):
-    print(f"\033[{color_code}m" + "=" * 42)
-    print(text.center(42, "="))
-    print("=" * 42)
-
 # create a program that will delete all duplicate characters in a string
 # pseudocode
 while True:
-      # define the string
-      input_str = input("\033[91mGood Day! Please enter the given string or 'charl' if it is done: ")
+    # define the string
+    input_str = input("\033[91mGood Day! Please enter the given string or 'charl' if it is done: ")
 
-      # to enter a new string or end the program
-      if input_str.lower() == 'charl':
-          break
+    # to enter a new string or end the program
+    if input_str.lower() == 'charl':
+        break
 
-      # initiate an empty string to store and delete duplication of characters
-      delete = ""
-      # establish the function
-      for char in input_str:
-          char_lower = char.lower()
-          if input_str.lower().count(char_lower) == 1:
-              delete += char
-      result = delete.replace(" ", "")
+    # initiate an empty string to store and delete duplication of characters
+    delete = ""
 
-      # run the program
-      print("\033[94m" + result)
+    # establish the function
+    for char in input_str:
+        char_lower = char.lower()
+        if input_str.lower().count(char_lower) == 1:
+            delete += char
+    result = delete.replace(" ", "")
 
-      # to end the loop
-      print("\033[92mYahoo!")
-      print_header("Laboratory Activity 01 ~ Python Review", 96)
+    # run the program
+    print("\033[94m" + result)
 
+# to end the loop
+print("\033[92mYahoo!")
