55c3cbaeaca0c338d05e5ea0a0b684c305a16901
Family <albon.136546120855@2depedqc.ph>
2023-10-02 12:26:05 +0800

    Drafting my scratch

--- print_diamond.py
+++ print_diamond.py
@@ -7,6 +7,22 @@ def print_header(text, color_code):
 
 # pseudocode
 # establish the function
+def print_diamond(n, color_code):
+
+    for i in range(1, n + 1, 2):
+        alignment = " " * ((n - i) // 2)
+        character = "*" * i
+        print(f"\033[{color_code}m{alignment}{character}{alignment}")
+
+    for i in range(n - 2, 0, -2):
+        alignment = " " * ((n - i) // 2)
+        character = "*" * i
+        print(f"\033[{color_code}m{alignment}{character}{alignment}")
+
+    if n % 2 == 0:
+        print(f"\033[94mPlease provide an odd integer.")
+        return
+    
 # run the program
 # end
 print_header("Laboratory Activity 01 ~ Python Review", 96)
