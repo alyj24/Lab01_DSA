82d907a8dd4b44be351a3189eecd15e26b102b46
Family <albon.136546120855@2depedqc.ph>
2023-10-02 12:38:56 +0800

    Construct

--- delete_duplication.py
+++ delete_duplication.py
@@ -5,12 +5,27 @@ def print_header(text, color_code):
 
 # create a program that will delete all duplicate characters in a string
 # pseudocode
+while True:
       # define the string
-      # to enter a new string or end the program
-      # initiate an empty string to store and delete duplication of characters
-      # establish the function
-      # run the program
-      # to end the loop
+      input_str = input("\033[91mGood Day! Please enter the given string or 'charl' if it is done: ")
 
-print_header("Laboratory Activity 01 ~ Python Review", 96)
+      # to enter a new string or end the program
+      if input_str.lower() == 'charl':
+          break
+
+      # initiate an empty string to store and delete duplication of characters
+      delete = ""
+      # establish the function
+      for char in input_str:
+          char_lower = char.lower()
+          if input_str.lower().count(char_lower) == 1:
+              delete += char
+      result = delete.replace(" ", "")
+
+      # run the program
+      print("\033[94m" + result)
+
+      # to end the loop
+      print("\033[92mYahoo!")
+      print_header("Laboratory Activity 01 ~ Python Review", 96)
 
