8a09b81de20f08fd7e49fb0fa4d094c39b651402
Family <albon.136546120855@2depedqc.ph>
2023-10-02 12:24:51 +0800

    pseudocode

--- print_diamond.py
+++ print_diamond.py
@@ -2,5 +2,11 @@ def print_header(text, color_code):
     print(f"\033[{color_code}m" + "=" * 42)
     print(text.center(42, "="))
     print("=" * 42)
+# write a python function named print_diamond that takes an odd integer n as an argument
+# and prints a diamond shape with a width of n using the * character.
 
+# pseudocode
+# establish the function
+# run the program
+# end
 print_header("Laboratory Activity 01 ~ Python Review", 96)
