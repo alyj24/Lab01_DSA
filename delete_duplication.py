906b7c6053b1499193fb7250dc71018bfa0ee85c
Family <albon.136546120855@2depedqc.ph>
2023-10-02 12:36:29 +0800

    Pseudocode

---
+++ delete_duplication.py
@@ -0,0 +1,16 @@
+def print_header(text, color_code):
+    print(f"\033[{color_code}m" + "=" * 42)
+    print(text.center(42, "="))
+    print("=" * 42)
+
+# create a program that will delete all duplicate characters in a string
+# pseudocode
+      # define the string
+      # to enter a new string or end the program
+      # initiate an empty string to store and delete duplication of characters
+      # establish the function
+      # run the program
+      # to end the loop
+
+print_header("Laboratory Activity 01 ~ Python Review", 96)
+
